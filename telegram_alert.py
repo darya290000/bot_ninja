4243f44e38d88c69255cbbaed70acc728959cb5a
darya290000 <darya290000@gmail.com>
2025-07-17 07:39:26 +0330

    telegram_alert.py

--- telegram_alert.py
+++ telegram_alert.py
@@ -4,16 +4,16 @@ from config import TOKEN, CHAT_ID
 def send_alert(message):
     url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
     data = {
-        'chat_id': CHAT_ID.strip(),  # حذف فاصله اضافی
+        'chat_id': CHAT_ID,
         'text': message,
-        'parse_mode': 'HTML'  # برای فرمت بهتر
+        'parse_mode': 'HTML'
     }
-    
+
     try:
         response = requests.post(url, data=data, timeout=10)
         if response.status_code == 200:
-            print(f"[✅] سیگنال ارسال شد: {message}")
+            print(f"[✅] پیام ارسال شد: {message}")
         else:
-            print(f"[❌] خطا در ارسال: {response.status_code}")
+            print(f"[❌] خطا در ارسال: {response.status_code}, {response.text}")
     except Exception as e:
-        print(f"[❌] خطا در ارسال تلگرام: {e}")
+        print(f"[❌] خطا در ارسال به تلگرام: {e}")
